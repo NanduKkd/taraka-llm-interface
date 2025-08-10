846436712375e956157f0c696aae21ea62753667
nandukkd <nandukkd7164@gmail.com>
2025-08-11 02:40:56 +0530

    fix: incorrect field matching

--- dao/session.ts
+++ dao/session.ts
@@ -12,7 +12,7 @@ export async function getSession(supabase: SupabaseClient, session_id: number) {
 export async function updateSession(supabase: SupabaseClient, session_id: number, session_title: string) {
   const { error } = await supabase.from('sessions').update({
     title: session_title
-  }).eq('session_id', session_id);
+  }).eq('id', session_id);
   if (error)
     throw error;
 }
